00061f9c98a041b0e8032329b2bf71d2c57d8092
brzydki <vl_silaev@mail.ru>
2021-12-08 14:24:42 +0300

    Add files via upload

---
+++ LAB_2_ZADANIE1.cpp
@@ -0,0 +1,34 @@
+﻿#include <time.h>
+#include <stdio.h>
+#include <math.h>
+#include <iostream>
+#include <iomanip>
+#include <clocale>
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+	srand(time(NULL));
+	int n, j, sum = 0;
+	cout << "Введите размер массива"; 
+	cin >> n;
+	int* arr = new int[n];
+	for (int i = 0; i < n; i++) {
+		arr[i] = rand() % 100 - 60 ;
+		cout << setw(3) << arr[i] << "  ";
+	}
+	int max = arr[0];
+	for (int i = 0; i < n; i++) {
+		if (arr[i] > max) {
+			max = arr[i];
+			j = i;
+			j++;
+		}
+	}
+	for (int i = j; i < n; i++) {
+		sum += arr[i];
+	}
+	cout << "\nМаксимальный элемент: " << max << endl;
+	cout << "Сумма элементов массива, стоящих после максимального элемента: " << sum << endl;
+	return 0;
+}
